3b6cf81c00d0e820d2bf6a20e3cf1448e2abffbb
K-0joo <83263735+K-0joo@users.noreply.github.com>
2022-01-22 15:06:07 +0900

    [No.3] BOJ10950 - A+B -3 / 구현
    - 문제 URL : https://www.acmicpc.net/problem/10950
    - 난이도 : 브론즈 3
    - 걸린 시간 : 2분

---
+++ K-0joo/BOJ/0_BOJ_10950.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int T = sc.nextInt();
+        
+        for(int i = 0; i < T; i++){
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            
+            int result = A+B;
+            
+            System.out.println(result);
+        }
+    }
+}
